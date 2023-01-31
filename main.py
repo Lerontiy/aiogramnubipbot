2b2c5d8dd3bab90133e9525693c50b0a726b6d22
Lerontiy <nazarenkoalexandr10@gmail.com>
2023-01-31 22:58:31 +0200

    Anya

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
         webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
-        skip_updates=True,
+        skip_updates=False,
         host=WEBAPP_HOST,
         port=int(os.environ.get("PORT", WEBAPP_PORT)),
     )
