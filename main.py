90e2e71bb595a6755eba82cf0d4cd895a8a1f538
Lerontiy <nazarenkoalexandr10@gmail.com>
2023-02-02 17:41:32 +0200

    .

--- main.py
+++ main.py
@@ -64,7 +64,16 @@ async def ggwp():
 
     
 async def main():
-    loop_.create_task(ggwp())
+    await loop_.create_task(start_webhook(
+                    dispatcher=dp,
+                    webhook_path=WEBHOOK_PATH,
+                    on_startup=on_startup,
+                    on_shutdown=on_shutdown,
+                    skip_updates=False,
+                    host=WEBAPP_HOST,
+                    port=int(os.environ.get("PORT", WEBAPP_PORT)),
+                    loop=loop_,
+                    ))
 
     print("main")
 
